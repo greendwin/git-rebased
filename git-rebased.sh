bae7027bae525b54dd3ecb03988c9463b23c2874
Evgeniy A. Cymbalyuk <cimbaluk@gmail.com>
2023-10-27 23:51:02 +0300

    add `pull` command

--- git-rebased.sh
+++ git-rebased.sh
@@ -50,10 +50,38 @@ cmd_push() {
     git push origin "$current:$backup"
 
     cprint $YELLOW "restore $GREEN$current"
-    git reset $current_hash
+    git reset "$current_hash"
 
     cprint $YELLOW "force push $GREEN$current"
     git push -f 
+
+    cprint $GREEN "done."
+}
+
+
+cmd_pull() {
+    # TODO: detect remote from current stream
+    local remote=origin
+    local current=$(git rev-parse --abbrev-ref HEAD)
+    local backup="backup/$current"
+    local current_hash=$(git rev-parse HEAD)
+
+    cprint $YELLOW "current hash is $BLUE$current_hash"
+
+    if ! git rev-parse --verify --quiet "$remote/$backup"; then
+        cprint $RED "backup branch $GREEN$remote/$backup$RESET was not found"
+        exit -1
+    fi
+
+    cprint $YELLOW "rebase on top of $GREEN$remote/$backup"
+    # merge both backup and main branch (they can differ)
+    git rebase "$remote/$backup"
+
+    cprint $YELLOW "move to mainline"
+    git rebase "$remote/$backup" --onto "$remote/$backup~"
+
+    cprint $YELLOW "rebase to $GREEN$remote/$current"
+    git rebase "$remote/$current"
 
     cprint $GREEN "done."
 }
@@ -79,12 +107,11 @@ usage() {
 }
 
 POS_ARGS=()
-DEBUG=0
 
 while [[ $# -gt 0 ]]; do
     case $1 in
         -d|--debug)
-            DEBUG=1
+            set -x
             shift
             ;;
         -h|--help)
@@ -116,6 +143,9 @@ fi
 
 if [[ "$1" == "push" ]]; then
     cmd_push
+    exit
+elif [[ "$1" == "pull" ]]; then
+    cmd_pull
     exit
 fi
 
